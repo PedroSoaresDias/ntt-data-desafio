f85175fa9f4fa3867e98e332b7e715fa65b01d7f
Pedro Soares Dias <pedroferraridias@gmail.com>
2025-08-10 13:05:37 -0300

    Configuração de perfis do application.properties em ambiente local e Docker

--- order-service/src/main/java/br/com/ntt/order_service/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/br/com/ntt/order_service/service/impl/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package br.com.ntt.order_service.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +16,17 @@ import br.com.ntt.order_service.service.OrderService;
 @Service
 public class OrderServiceImpl implements OrderService {
     private final RestTemplate restTemplate;
+    private final String productServiceUrl;
 
-    public OrderServiceImpl(RestTemplate restTemplate) {
+    public OrderServiceImpl(RestTemplate restTemplate, @Value("${product.service.url}") String productServiceUrl) {
         this.restTemplate = restTemplate;
+        this.productServiceUrl = productServiceUrl;
     }
 
     @Override
     public OrderResponse simulateOrder(List<Long> productIds) {
         String idsParam = String.join(",", productIds.stream().map(String::valueOf).toList());
-        String url = "http://localhost:8181/products/filter?ids=" + idsParam;
+        String url = productServiceUrl + "/products/filter?ids=" + idsParam;
 
         ResponseEntity<List<ProductDTO>> response = restTemplate.exchange(
                 url,
